faf79497617dc4565de1a4ab0b1f8158c66c8c79
Oliver Bertuch <o.bertuch@fz-juelich.de>
2020-03-30 01:50:46 +0200

    Commit initial (untested) version of code.

---
+++ inkscape-ext-pdflayers.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python
+# coding=utf-8
+#
+# Copyright 2020 Oliver Bertuch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+"""
+Export layers to a multipage PDF file, using locked layers as backgrounds
+for every page, adding one layer at a time.
+"""
+
+import inkex
+from inkex.base import TempDirMixin
+from inkex.command import take_snapshot
+
+import PyPDF2
+
+class Layers_Multipage_PDF_Export(TempFileMixin, inkex.OutputExtension):
+
+    dir_prefix = 'multipagePDF-'
+
+    def save(self, stream):
+        # Init PDF merger
+        pdf_merger = PyPDF2.PdfFileMerger()
+
+        # Find layers.
+        bg_layers = self.svg.xpath("//svg:g[@inkscape:groupmode='layer' and @sodipodi:insensitive='true']")
+        layers = self.svg.xpath("//svg:g[@inkscape:groupmode='layer' and not(@sodipodi:insensitive)]")
+
+        if len(layers) + len(bg_layers) < 1:
+            inkex.errormsg("No layers found.")
+            return
+
+        for node in layers:
+            # Make all layers invisible
+            node.style['display'] = "none"
+
+        # Iterate the layers, save to PDF.
+        for node in layers:
+            # Show only one layer at a time.
+            node.style.update("display:inherit;opacity:1")
+
+            name = node.get('inkscape:label')
+            newname = "{}.{}".format(name, self.options.type)
+            # Save snapshot as PDF export in temp file
+            filename = take_snapshot(self.document, dirname=self.tempdir,
+                                        name=name, ext='pdf')
+            # Append temp PDF to merged PDF
+            pdf_merger.append(filename, bookmark=name)
+
+            node.style['display'] = "none"
+
+        # Write out merged PDF to destination
+        pdf_merger.write(stream)
+
+
+if __name__ == '__main__':
+    Layers_Multipage_PDF_Export().run()
---
+++ setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+# The get_description function takes the local README.md and adds on the
+# local icon.svg to the bottom of the string, this is then recovered
+# later for display within the extension manager.
+
+
+# from inkman.utils import get_description
+
+setup(
+    # Give your extension a good and unique name.
+    name='inkscape-ext-pdflayers',
+
+    # Your version should reflect the maturity of your extension
+    # as well as optionally be linked to the version of a dependnant package
+    #
+    # 0.1 - 0.3 : Alpha level, expected to fail
+    # 0.4 - 0.8 : Beta level, some functionality is expected to fail
+    #       0.9 : Pre-release level, everything should work, just fixing some things
+    #       1.0 : Finished thing, all the features are working, all non working items
+    #             have been removed.
+    #       2.0 : The next version or release after 1.0
+    #
+    version='0.1',
+
+    # This is the short summary which appears below the name in the extensions manager.
+    # Try not to make this too long, use the long-description (in README.md).
+    description='Write layered SVG images as multipage PDF.',
+
+    # This shouldn't be changed
+    #long_description=get_description(__file__),
+
+    url='https://github.com/poikilotherm/inkscape-ext-pdflayers',
+    author='Oliver Bertuch',
+    author_email='oliver@bertuch.eu',
+
+    # Licenses, see classifiers
+    license='GPLv3',
+
+    # Make sure to include all the inx files, place the in the inx directory
+    data_files=[('inx', ['inkscape-ext-pdflayers.inx'])],
+
+    # Include any of the extension scripts, remember they will end up in the
+    # bin directory of the deployment. So:
+    # ~/.config/inkscape/extensions/bin/my-extension.py
+    # Which will be referenced in the inx file as bin/my-extension.py
+    scripts=['inkscape-ext-pdflayers.py'],
+
+    # Some extensions are more than just a script; the include python module
+    # these modules are specified here, they get installed into the directory
+    # ~/.config/inkscape/extensions/lib/python3.5/site-packages/
+    # And should be available to your scripts after importing inkpath
+    packages=[],
+
+    # If modules (above) also include non-python files, such as images
+    # and other data. Then this should be set to True.
+    # Make sure the module data is included in the MANIFEST.in file.
+    include_package_data=False,
+
+    classifiers=[
+        # These are REQUIRED for this to be detected as a valid inkscape extension
+        'Environment :: Plugins',
+        'Topic :: Multimedia :: Graphics :: Editors :: Vector-Based',
+
+        # These are optional and depend on your requirements
+        'Intended Audience :: Other Audience',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    python_requires='>=3.5',
+    install_requires=[
+        'PyPDF2==1.26.0'
+    ],
+)
